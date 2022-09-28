56812a0c6045d39bc9a19b196b23ad82d9099015
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-09-28 22:55:50 +0300

    добавил ui

--- src/main/java/dark/SpriteX.java
+++ src/main/java/dark/SpriteX.java
@@ -4,33 +4,47 @@ import arc.ApplicationListener;
 import arc.Core;
 import arc.assets.AssetManager;
 import arc.graphics.Color;
+import arc.graphics.g2d.SortedSpriteBatch;
 import arc.scene.Scene;
 import arc.util.Log;
+import arc.util.Time;
 import dark.ui.Fonts;
+import dark.ui.UI;
 
 import static arc.Core.*;
 
 public class SpriteX implements ApplicationListener {
 
+    public static boolean finished;
+
     @Override
     public void init() {
+        Time.mark();
+
+        batch = new SortedSpriteBatch();
         scene = new Scene();
         assets = new AssetManager();
 
-        Fonts.load();
-        assets.update(50);
+        Fonts.load();            
 
-        Log.infoTag("SPRITEX", "Initialized");
+        Log.infoTag("APP", "Initialized");
     }
 
     @Override
     public void dispose() {
-        Log.infoTag("SPRITEX", "Disposed");
+        Log.infoTag("APP", "Disposed");
     }
 
     @Override
     public void update() {
-        Core.graphics.clear(Color.blue);
-        Core.scene.draw();
+        if (finished) {
+            Core.graphics.clear(Color.blue);
+            Core.scene.draw();
+        } else if (assets.update(50)) {
+            UI.load();
+
+            finished = true;
+            Log.info("Total time to load: @ms", Time.elapsed());
+        }
     }
 }
