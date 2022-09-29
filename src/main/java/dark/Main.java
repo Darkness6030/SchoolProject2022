1568f84a0711775b21550acb4d92547d184ff51e
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-09-29 17:24:48 +0300

    добавил красивое логирование

--- src/main/java/dark/Main.java
+++ src/main/java/dark/Main.java
@@ -3,10 +3,22 @@ package dark;
 import arc.Files.FileType;
 import arc.backend.sdl.SdlApplication;
 import arc.backend.sdl.SdlConfig;
+import arc.util.Log;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
+    public static final String[] tags = { "&lc&fb[D]&fr", "&lb&fb[I]&fr", "&ly&fb[W]&fr", "&lr&fb[E]", "" };
+    public static final DateTimeFormatter dateTime = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
     public static void main(String[] args) {
+        Log.logger = (level, text) -> {
+            String result = Log.format("&lk&fb[" + dateTime.format(LocalDateTime.now()) + "]&fr " + tags[level.ordinal()] + " " + text + "&fr");
+            System.out.println(result);
+        };
+
         new SdlApplication(new SpriteX(), new SdlConfig() {{
             title = "SpriteX";
             maximized = true;
