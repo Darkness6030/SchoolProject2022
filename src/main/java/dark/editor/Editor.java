f45ca6bb88ed1a79b53505379fcad9d88fabd056
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-09-30 18:17:12 +0300

    добавил передвижение камеры мышкой + скм

--- src/main/java/dark/editor/Editor.java
+++ src/main/java/dark/editor/Editor.java
@@ -7,17 +7,23 @@ import arc.graphics.g2d.Fill;
 import arc.graphics.g2d.Lines;
 import arc.graphics.g2d.TextureRegion;
 import arc.graphics.gl.FrameBuffer;
+import arc.input.GestureDetector;
+import arc.input.GestureDetector.GestureListener;
 import arc.input.KeyCode;
 import arc.math.geom.Vec2;
 
 import static arc.Core.*;
 
-public class Editor implements ApplicationListener {
+public class Editor implements ApplicationListener, GestureListener {
 
     public static final Vec2 camera = new Vec2();
 
     public FrameBuffer buffer = new FrameBuffer(800, 600); // temp
     public EditType type = EditType.pencil;
+
+    public Editor() {
+        input.addProcessor(new GestureDetector(this));
+    }
 
     @Override
     public void update() {
@@ -30,6 +36,15 @@ public class Editor implements ApplicationListener {
         else if (input.keyDown(KeyCode.a)) camera.x -= 1f;
         if (input.keyDown(KeyCode.w)) camera.y += 1f;
         else if (input.keyDown(KeyCode.s)) camera.y -= 1f;
+    }
+
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
+        if (input.keyDown(KeyCode.mouseMiddle)){
+            camera.x -= deltaX;
+            camera.y -= deltaY;
+        }
+        return false;
     }
 
     public void draw() {
