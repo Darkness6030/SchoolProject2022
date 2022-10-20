37c83391930ff649237ee0a553337ac48a915d3e
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-10-20 17:09:58 +0300

    добавил красивое отображение палитры и пару иконок

--- src/main/java/dark/ui/Icons.java
+++ src/main/java/dark/ui/Icons.java
@@ -6,6 +6,10 @@ public class Icons {
     public static char pick = '\uE877';
     public static char eraser = '\uF12D';
     public static char line = '\uE82B';
-    public static char back = '\uE802';
+
     public static char ok = '\uE800';
+    public static char back = '\uE802';
+    public static char exit = '\uE85F';
+
+    public static char swap = '\uE823';
 }
--- src/main/java/dark/ui/fragments/HudFragment.java
+++ src/main/java/dark/ui/fragments/HudFragment.java
@@ -1,7 +1,10 @@
 package dark.ui.fragments;
 
+import arc.graphics.Color;
+import arc.scene.ui.layout.Table;
 import arc.scene.ui.layout.WidgetGroup;
 import dark.editor.EditType;
+import dark.ui.Icons;
 import dark.ui.Textures;
 import dark.ui.elements.TextSlider;
 
@@ -19,10 +22,26 @@ public class HudFragment {
 
                 pad.button("Menu", () -> ui.menuDialog.show()).size(64f);
 
-                new TextSlider(1f, 100f, 1f, 2f, value -> (editor.drawSize = value.intValue()) + "px").build(pad);
+                new TextSlider(1f, 100f, 1f, 2f, value -> (editor.drawSize = value.intValue()) + "px").build(pad).padRight(16f);
 
-                pad.button(Textures.color_blob, () -> {}).update(button -> button.getImage().setColor(editor.first));
-                pad.button(Textures.color_blob, () -> {}).update(button -> button.getImage().setColor(editor.second));
+                class ColorBlob extends Table {
+
+                    public ColorBlob(Color color, float x, float y) {
+                        button(Textures.color_blob, () -> { /* TODO color pick dialog */ })
+                                .with(button -> button.setTranslation(x, y))
+                                .update(button -> button.getImage().setColor(color));
+                    }
+                }
+
+                pad.stack(
+                    new Table(table -> table.button(String.valueOf(Icons.swap), () -> {
+                        var temp = editor.first.cpy();
+                        editor.first.set(editor.second);
+                        editor.second.set(temp);
+                    }).size(16f).with(button -> button.setTranslation(18f, 18f))),
+                    new ColorBlob(editor.second, 8f, -8f),
+                    new ColorBlob(editor.first, -8f, 8f)
+                ).size(64f);
             }).height(64f).growX();
         });
 
