a4cf210b728f577cad22c28e1a6f409390d35391
xzxADIxzx <valerazhuk20122006@gmail.com>
2023-03-06 22:26:21 +0300

    загрузка шрифтов через генератор

--- src/main/java/dark/ui/Fonts.java
+++ src/main/java/dark/ui/Fonts.java
@@ -2,42 +2,35 @@ package dark.ui;
 
 import arc.freetype.*;
 import arc.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
-import arc.freetype.FreetypeFontLoader.FreeTypeFontLoaderParameter;
 import arc.graphics.Color;
-import arc.graphics.Gl;
 import arc.graphics.g2d.Font;
-import arc.graphics.g2d.PixmapPacker;
 
 import static arc.Core.*;
 
 public class Fonts {
 
-    public static PixmapPacker packer;
     public static Font def, icon;
 
     public static void load() {
-        packer = new PixmapPacker(Gl.getInt(Gl.maxTextureSize) >= 4096 ? 4096 : 2048, 2048, 2, true);
-
-        assets.setLoader(FreeTypeFontGenerator.class, new FreeTypeFontGeneratorLoader(files::internal));
-        assets.setLoader(Font.class, null, new FreetypeFontLoader(files::internal));
-
-        assets.load("font", Font.class, new FreeTypeFontLoaderParameter("fonts/font.woff", new FreeTypeFontParameter() {{
+        def = load("font.woff", new FreeTypeFontParameter() {{
             size = 18;
             shadowColor = Color.darkGray;
             shadowOffsetY = 2;
             incremental = true;
-        }})).loaded = font -> {
-            font.getData().markupEnabled = true;
-            def = font;
-        };
+        }});
+        def.getData().markupEnabled = true;
 
-        assets.load("icon", Font.class, new FreeTypeFontLoaderParameter("fonts/icon.ttf", new FreeTypeFontParameter() {{
-            size = 30;
-            characters = "\0";
+        icon = load("font.woff", new FreeTypeFontParameter() {{
+            size = 18;
+            shadowColor = Color.darkGray;
+            shadowOffsetY = 2;
             incremental = true;
-        }})).loaded = font -> {
-            font.getData().markupEnabled = true;
-            icon = font;
-        };
+        }});
+        icon.getData().markupEnabled = true;
+    }
+
+    public static Font load(String name, FreeTypeFontParameter parameter) {
+        var generator = new FreeTypeFontGenerator(files.internal("fonts/" + name));
+        return generator.generateFont(parameter);
     }
 }
