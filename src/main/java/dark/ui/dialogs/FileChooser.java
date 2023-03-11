f571e3b77291a41950acaa1758d714c744d16dd8
xzxADIxzx <valerazhuk20122006@gmail.com>
2023-03-11 12:44:36 +0300

    зарезал file chooser

--- src/main/java/dark/ui/dialogs/FileChooser.java
+++ src/main/java/dark/ui/dialogs/FileChooser.java
@@ -2,191 +2,157 @@ package dark.ui.dialogs;
 
 import arc.files.Fi;
 import arc.func.Cons;
-import arc.scene.event.Touchable;
-import arc.scene.ui.*;
+import arc.scene.ui.TextButton;
+import arc.scene.ui.TextField;
 import arc.scene.ui.layout.Table;
 import arc.struct.Seq;
 import arc.util.Align;
 import dark.ui.Icons;
-import dark.ui.Styles;
 import dark.ui.elements.FocusScrollPane;
-
-import java.util.Comparator;
 
 import static arc.Core.*;
 
+import java.util.Comparator;
+
 public class FileChooser extends BaseDialog {
-    public static final Fi homeDirectory = files.absolute(files.getExternalStoragePath());
 
-    public Fi directory = files.absolute(settings.getString("lastDirectory", homeDirectory.absolutePath()));
-    public Table table;
+    public static final String externalStorage = files.getExternalStoragePath();
+    public static final Fi homeDirectory = files.absolute(externalStorage);
+
+    public FileHistory history = new FileHistory();
+    public Fi directory = files.absolute(settings.getString("last-dir", externalStorage));
+
+    public boolean open;
+    public String extension;
+
+    public Table list;
     public FocusScrollPane pane;
-    public TextField field, navigation;
+    public TextField field;
 
-    public final FileHistory history = new FileHistory();
-
-    public final boolean open;
-    public final String extension;
-    public final Cons<Fi> result;
-
-    public FileChooser(String title, boolean open, String extension, Cons<Fi> result) {
+    public FileChooser(String title, boolean open, String extension, Cons<Fi> cons) {
         super(title);
-        setFillParent(true);
+        addCloseButton();
 
         this.open = open;
         this.extension = extension;
-        this.result = result;
 
-        if (!directory.exists())
-            directory = homeDirectory;
+        cont.table(nav -> {
+            nav.defaults().size(48f).padRight(8f);
 
-        shown(() -> {
-            cont.clear();
-            setupWidgets();
-        });
+            nav.button(Icons.home, this::openHomeDirectory);
+            nav.button(Icons.left, history::back).disabled(b -> history.noBack());
+            nav.button(Icons.right, history::forward).disabled(b -> history.noForward());
+            nav.button(Icons.up, this::openParentDirectory);
 
-        closeOnBack();
-    }
+            nav.label(() -> directory.absolutePath()).ellipsis(true).width(318f);
+        }).growX().row();
 
-    public void setupWidgets() {
-        table = new Table();
-        table.marginRight(10f);
-        table.marginLeft(3f);
+        list = new Table();
+        list.top();
 
-        pane = new FocusScrollPane(table);
-        pane.setOverscroll(false, false);
+        pane = new FocusScrollPane(list);
+        pane.setOverscroll(true, true);
         pane.setFadeScrollBars(false);
 
-        field = new TextField();
-        field.setOnlyFontChars(false);
-        field.setDisabled(open);
-        field.setMaxLength(128);
-        field.setFilter((field, text) -> text != ' ');
-        field.setMessageText("@empty");
+        cont.add(pane).size(550f, 700f).row();
+        cont.table(name -> {
+            name.left();
 
-        navigation = new TextField();
-        navigation.touchable = Touchable.disabled;
+            field = new TextField();
+            field.setOnlyFontChars(false);
+            field.setDisabled(open);
+            field.setMaxLength(128);
+            field.setMessageText("@empty");
 
-        updateFiles(true);
+            name.add("@file.name");
+            name.add(field).grow().padLeft(8f);
+        }).growX();
 
-        var icons = new Table();
-        icons.defaults().growX().height(60f).padTop(5f).uniform();
+        buttons.buttonRow("@ok", Icons.ok, () -> {
+            Fi fi = directory.child(name());
+            cons.get(open ? fi : fi.sibling(fi.nameWithoutExtension() + "." + extension));
 
-        icons.button(Icons.home, this::openHomeDirectory);
-        icons.button(Icons.left, history::back).disabled(button -> history.noBack());
-        icons.button(Icons.right, history::forward).disabled(button -> history.noForward());
-        icons.button(Icons.up, this::openParentDirectory);
-
-        var fileName = new Table();
-        fileName.bottom().left().add(new Label("@file.name"));
-        fileName.add(field).height(40f).fillX().expandX().padLeft(12f);
-
-        var buttons = new Table();
-        buttons.defaults().grow().height(60f);
-        buttons.button("@cancel", this::hide);
-        buttons.button("@ok", () -> {
             hide();
+        }).disabled(button -> open ? !directory.child(name()).exists() || directory.child(name()).isDirectory() : name().trim().isEmpty());
 
-            var fi = directory.child(field.getText());
-            result.get(open ? fi : fi.sibling(fi.nameWithoutExtension() + "." + extension));
-        }).disabled(button -> open ? !directory.child(field.getText()).exists() || directory.child(field.getText()).isDirectory() : field.getText().trim().isEmpty());
-
-        var content = new Table();
-        content.top().left().add(icons).growX();
-        content.row();
-
-        content.center().add(pane).colspan(3).grow();
-        content.row();
-
-        content.bottom().left().add(fileName).colspan(3).grow().padTop(-2f).padBottom(2f);
-        content.row();
-
-        content.add(buttons).growX();
-        cont.add(content).grow();
+        if (!directory.exists()) directory = homeDirectory;
+        updateFiles(true);
     }
+
+    public String name() {
+        return field.getText();
+    }
+
+    // region files
 
     public Seq<Fi> getAvailableFiles() {
         return directory.seq()
+                .removeAll(fi -> fi.name().startsWith(".")) // убирает скрытые файлы
                 .filter(fi -> fi.isDirectory() || fi.extEquals(extension))
                 .sort(Comparator.comparing(Fi::isDirectory))
                 .sort(Comparator.comparing(Fi::name));
     }
 
-    public void openHomeDirectory() {
-        directory = homeDirectory;
-        settings.put("lastDirectory", directory.absolutePath());
-
-        updateFiles(true);
-    }
-
-    public void openParentDirectory() {
-        directory = directory.parent();
-        settings.put("lastDirectory", directory.absolutePath());
-
-        updateFiles(true);
-    }
-
-    public void openChildDirectory(String name) {
-        directory = directory.child(name);
-        settings.put("lastDirectory", directory.absolutePath());
-
-        updateFiles(true);
-    }
-
     public void updateFiles(boolean push) {
         if (push) history.push(directory);
-        navigation.setText(directory.toString());
+        settings.put("last-dir", directory.absolutePath());
 
-        table.clear();
-        table.top().left();
+        list.clear();
+        list.defaults().height(48f).growX();
 
-        table.button(".." + directory.toString(), Styles.textButtonCheck, this::openParentDirectory).with(button -> {
-            button.image(Icons.up).padLeft(4f).padRight(4f);
-            button.getLabel().setAlignment(Align.left);
-            button.getCells().reverse();
-        }).align(Align.topLeft).colspan(2).growX().height(50f).pad(2f);
-
-        table.row();
+        Cons<TextButton> labelAlign = b -> b.getLabelCell().padLeft(8f).labelAlign(Align.left);
+        list.button(".." + directory.toString(), Icons.up, this::openParentDirectory).with(labelAlign).row();
 
         getAvailableFiles().each(fi -> {
-            table.button(fi.name().replace("[", "[["), Styles.textButtonCheck, () -> {
-                if (fi.isDirectory()) openChildDirectory(fi.name());
-                else field.setText(fi.name());
-            }).with(button -> {
-                button.image(fi.isDirectory() ? Icons.folder : Icons.file).padLeft(4f).padRight(4f);
-                button.getLabel().setAlignment(Align.left);
-                button.getCells().reverse();
-            }).checked(button -> field.getText().equals(fi.name()))
-                    .align(Align.topLeft).colspan(2).growX()
-                    .height(50f).padLeft(2f).padRight(2f);
-
-            table.row();
+            list.button(fi.name().replace("[", "[["), fi.isDirectory() ? Icons.folder : Icons.file, () -> {
+                if (fi.isDirectory())
+                    openChildDirectory(fi.name());
+                else
+                    field.setText(fi.name());
+            }).checked(b -> name().equals(fi.name())).with(labelAlign).row();
         });
 
         pane.setScrollY(0f);
         if (open) field.clearText();
     }
 
+    // endregion
+    // region directories
+
+    public void openHomeDirectory() {
+        directory = homeDirectory;
+        updateFiles(true);
+    }
+
+    public void openParentDirectory() {
+        directory = directory.parent();
+        updateFiles(true);
+    }
+
+    public void openChildDirectory(String name) {
+        directory = directory.child(name);
+        updateFiles(true);
+    }
+
+    // endregion
+
     public class FileHistory extends Seq<Fi> {
+
         public int index;
 
         public void push(Fi file) {
-            if (index != size) truncate(index);
+            if (index != size) truncate(index); // новая папка открыта не из конца истории
 
             add(file);
             index++;
         }
 
         public void back() {
-            if (noBack()) return;
-
             directory = get(--index - 1);
             updateFiles(false);
         }
 
         public void forward() {
-            if (noForward()) return;
-
             directory = get(index++);
             updateFiles(false);
         }
