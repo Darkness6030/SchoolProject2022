2c47bcfc934aebdac1db7b41633d61fe0446e882
xzxADIxzx <valerazhuk20122006@gmail.com>
2023-03-18 13:31:39 +0300

    не ну ты молодец кнч

--- src/main/java/dark/ui/elements/Switch.java
+++ src/main/java/dark/ui/elements/Switch.java
@@ -22,7 +22,7 @@ public class Switch extends CheckBox {
         this.unchecked = unchecked;
 
         this.changed(() -> listener.get(isChecked()));
-        this.getImage().setColor(Palette.darkmain);
+        this.getImage().setColor(unchecked);
     }
 
     @Override
